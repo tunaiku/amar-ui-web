7743cbe0f462511edbad9fb58c491c9861a5cb63
dkk94 <davidkurniakristiadi@gmail.com>
2020-03-05 21:15:41 +0700

    chore: remove tnk-* and tunaiku-* namespace of all affected directories

--- library/button/CHANGELOG.md
+++ library/button/CHANGELOG.md
@@ -5,4 +5,4 @@ See [Conventional Commits](https://conventionalcommits.org) for commit guideline
 
 ## 1.0.1 (2020-02-23)
 
-**Note:** Version bump only for package @tunaiku-ui/button
+**Note:** Version bump only for package @amar-ui-web/button
--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Tunaiku UI`,
+    title: `Amar UI Components`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `David Kurnia Kristiadi @dkk94`,
   },
