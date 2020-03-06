f34481400ccfe6495fff1b633b1207d782bc36f1
dkk94 <davidkurniakristiadi@gmail.com>
2020-03-07 03:35:09 +0700

    chore: rename amar-ui-web to amar-web-ui

--- packages/docs/src/pages/index.js
+++ packages/docs/src/pages/index.js
@@ -7,102 +7,60 @@ const IndexPage = () => (
   <MainLayout>
     <SEO title="Home" />
     <div className="HomePage">
-      <section className="BgColor-primary Color-light PaddingTop-3xlarge PaddingBottom-3xlarge">
+      <section className="PaddingTop-3xlarge PaddingBottom-3xlarge">
         <div className="Container">
           <div className="Hero-content">
             <div className="MarginBottom-xlarge">
-              <h1 className="Heading-1">Tunaiku UI Library</h1>
-              <p>
-                Sint nulla ea officia incididunt in nisi id adipisicing reprehenderit adipisicing
-                quis.
+              <h1 className="Heading-1">Amar Web UI</h1>
+              <p className="Color-neutral-80">
+                Documentation of style guidelines used for crafting beautiful components that adhere
+                to Amar Design System.
               </p>
             </div>
-            <button className="Button Button--light Button--outlined">See Docs</button>
+            <button className="Button Button--primary Button--elevated">See Documentation</button>
           </div>
         </div>
       </section>
-      <section className="Section PaddingTop-2xlarge PaddingBottom-2xlarge">
+      <section className="PaddingTop-xlarge PaddingBottom-xlarge">
         <div className="Container">
-          <h2 className="Heading-2 MarginBottom-xlarge">Buttons</h2>
-          <section className="MarginBottom">
-            <h3 className="Heading-6 MarginBottom-2xsmall">Themed</h3>
-            <button className="Button MarginRight">Default</button>
-            <button className="Button Button--primary MarginRight">Primary</button>
-            <button className="Button Button--secondary">Secondary</button>
-          </section>
-          <section className="MarginBottom">
-            <h3 className="Heading-6 MarginBottom-2xsmall">Size</h3>
-            <button className="Button Button--small Button--primary MarginRight">Small</button>
-            <button className="Button Button--primary MarginRight">Default</button>
-            <button className="Button Button--large Button--primary">Large</button>
-          </section>
-          <section>
-            <h3 className="Heading-6 MarginBottom-2xsmall">With Elevation</h3>
-            <button className="Button Button--primary Button--elevated">Elevated</button>
-          </section>
-        </div>
-      </section>
-      <section className="Section PaddingTop-2xlarge PaddingBottom-2xlarge">
-        <div className="Container">
-          <h2 className="Heading-2 MarginBottom-xlarge">Cards</h2>
-          <section className="MarginBottom">
-            <h3 className="Heading-5 MarginBottom-2xsmall">Example</h3>
-            <div className="Grids">
-              <div className="Grid-12 Grid-md-6 Grid-xl-4">
-                <div className="Card Card--elevated">
-                  <div className="Card-block">
-                    <div className="MarginBottom-large">
-                      <div className="FontSize-xlarge FontWeight-bold">Tagihan Saat Ini</div>
-                      <div className="Color-gray-50">
-                        Segera bayar tagihan untuk menghindari tunggakan biaya.
-                      </div>
-                    </div>
-                  </div>
-                  <div className="Card-block">
-                    <div className="MarginBottom Flex JustifyContent-between Color-gray-50">
-                      <div>Tagihan pokok bulan ini:</div>
-                      <div className="FontWeight-bold">Rp 1.299.000</div>
-                    </div>
-                    <div className="Flex JustifyContent-between Color-gray-50">
-                      <div>Jatuh tempo:</div>
-                      <div className="FontWeight-bold">7 Februari 2020</div>
-                    </div>
-                  </div>
-                  <div className="Card-block">
-                    <div className="Flex JustifyContent-between">
-                      <div className="FontSize-large FontWeight-bold Color-gray-50">
-                        Total Tagihan:
-                      </div>
-                      <div className="FontSize-large FontWeight-bold">Rp 1.299.000</div>
-                    </div>
-                  </div>
-                  <div className="Card-block Flex JustifyContent-between">
-                    <button className="Button Button--large Button--primary Button--fullWidth">
-                      Pay with TunaikuPay
-                    </button>
-                  </div>
+          <div className="Grids">
+            <div className="Grid Grid-4 Flex FlexGrow-1">
+              <div className="Card Card--elevated Flex FlexGrow-1">
+                <div className="Card-block FlexGrow-1">
+                  <h5 className="Heading-5 MarginBottom-2xsmall">Get Started</h5>
+                  <p>Get the correct tools, see installation step-by-step.</p>
+                </div>
+                <div className="Card-block Flex JustifyContent-end">
+                  <button className="Button Button--small Button--primary">Go to docs</button>
                 </div>
               </div>
             </div>
-          </section>
-          <section className="Section PaddingTop-2xlarge PaddingBottom-2xlarge">
-            <h2 className="Heading-2 MarginBottom-xlarge">Banners</h2>
-            <div className="BgColor-lighter Padding-large">
-              <div className="Banner Banner--white Color-gray-50">
-                <div className="Banner-block MarginBottom">
-                  <p>Ullamco dolore tempor aliqua magna nostrud quis anim commodo.</p>
+            <div className="Grid Grid-4 Flex FlexGrow-1">
+              <div className="Card Card--elevated Flex FlexGrow-1">
+                <div className="Card-block FlexGrow-1">
+                  <h5 className="Heading-5 MarginBottom-2xsmall">Components</h5>
+                  <p>From buttons, cards to bottom sheet, we have all it covered!</p>
                 </div>
-                <div className="Banner-actions Flex JustifyContent-end">
-                  <button className="Button Button--text Button--secondary Text-uppercase FontSize-small Tracking-loose MarginRight-2xsmall">
-                    Action 1
-                  </button>
-                  <button className="Button Button--text Button--secondary Text-uppercase FontSize-small Tracking-loose">
-                    Action 2
-                  </button>
+                <div className="Card-block Flex JustifyContent-end">
+                  <button className="Button Button--small Button--primary">Go to docs</button>
                 </div>
               </div>
             </div>
-          </section>
+            <div className="Grid Grid-4">
+              <div className="Card Card--elevated">
+                <div className="Card-block">
+                  <h5 className="Heading-5 MarginBottom-2xsmall">Layouts</h5>
+                  <p>
+                    Several APIs to help you structure your layouts easily to make sure they don't
+                    crumble apart!
+                  </p>
+                </div>
+                <div className="Card-block Flex JustifyContent-end">
+                  <button className="Button Button--small Button--primary">Go to docs</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
