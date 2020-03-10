a0d1c6f1a7b6a0c61fdfe73218486187ef0167ef
dkk94 <davidkurniakristiadi@gmail.com>
2020-03-10 16:28:40 +0700

    chore(conventional-commit): configure initial commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  rules: {
+    'references-empty': [2, 'never'],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'improvement',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
+};
