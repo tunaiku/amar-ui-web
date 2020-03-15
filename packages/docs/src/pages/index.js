f4b5f2c0b3dea127f7631d5e0c3e37127594a234
dkk94 <davidkurniakristiadi@gmail.com>
2020-03-15 23:34:33 +0700

    docs: improve wording on home

--- packages/docs/src/pages/index.js
+++ packages/docs/src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import MainLayout from '../layouts/main-layout';
+import { MainLayout } from 'layouts/main-layout';
 import { SEO } from '../components/seo';
 
 const IndexPage = () => (
@@ -10,14 +10,18 @@ const IndexPage = () => (
       <section className="PaddingTop-3xlarge PaddingBottom-3xlarge">
         <div className="Container">
           <div className="Hero-content">
-            <div className="MarginBottom-xlarge">
-              <h1 className="Heading-1">Amar Web UI</h1>
-              <p className="Color-neutral-80">
-                Documentation of style guidelines used for crafting beautiful components that adhere
-                to Amar Design System.
+            <div className="MarginBottom-xlarge" style={{ width: '640px' }}>
+              <h1 className="Heading-1">Amar UI Web</h1>
+              <p className="Heading-4 Color-neutral-80">
+                CSS Framework for crafting beautiful UI components and layouts based on Amar Design
+                System.
               </p>
             </div>
-            <button className="Button Button--primary Button--elevated">See Documentation</button>
+            <div className="Flex">
+              <button className="MarginRight-small Button Button--primary Button--elevated">
+                Let's get started!
+              </button>
+            </div>
           </div>
         </div>
       </section>
@@ -39,7 +43,7 @@ const IndexPage = () => (
               <div className="Card Card--elevated Flex FlexGrow-1">
                 <div className="Card-block FlexGrow-1">
                   <h5 className="Heading-5 MarginBottom-2xsmall">Components</h5>
-                  <p>From buttons, cards to bottom sheet, we have all it covered!</p>
+                  <p>From buttons, cards to bottom sheet, we have it all covered!</p>
                 </div>
                 <div className="Card-block Flex JustifyContent-end">
                   <button className="Button Button--small Button--primary">Go to docs</button>
