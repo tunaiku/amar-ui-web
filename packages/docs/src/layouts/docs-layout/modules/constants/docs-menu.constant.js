a814eeb20227eb10b983ec31cedb80d5da63a264
dkk94 <davidkurniakristiadi@gmail.com>
2020-03-20 23:44:17 +0700

    refactor(docs): move menu object array to constant instead

---
+++ packages/docs/src/layouts/docs-layout/modules/constants/docs-menu.constant.js
@@ -0,0 +1,60 @@
+export const DOCS_MENU = [
+  {
+    title: 'Getting Started',
+    path: '/getting-started',
+  },
+  {
+    title: 'Components',
+    path: '/components',
+    submenu: [
+      {
+        title: 'Card',
+        path: '/components/card',
+      },
+      {
+        title: 'Button',
+        path: '/components/button',
+      },
+      {
+        title: 'Top Bar',
+        path: '/components/top-bar',
+      },
+      {
+        title: 'Bottom Bar',
+        path: '/components/bottom-bar',
+      },
+      {
+        title: 'Color',
+        path: '/components/color',
+      },
+      {
+        title: 'Field',
+        path: '/components/field',
+      },
+      {
+        title: 'Dialog',
+        path: '/components/dialog',
+      },
+      {
+        title: 'Bottom Sheet',
+        path: '/components/bottom-sheet',
+      },
+      {
+        title: 'Snackbar',
+        path: '/components/snackbar',
+      },
+      {
+        title: 'Radio',
+        path: '/components/radio',
+      },
+      {
+        title: 'Checkbox',
+        path: '/components/checkbox',
+      },
+      {
+        title: 'Typography',
+        path: '/components/typography',
+      },
+    ],
+  },
+];
