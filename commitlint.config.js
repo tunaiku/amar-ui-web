6707eec6322d4b04c7f29c8fcf5286defff49e96
dkk94 <davidkurniakristiadi@gmail.com>
2020-04-04 02:42:45 +0700

    fix(button): add text-decoration for linked button
    To remove underlined text-decoration if .Button is applied inside link tag.

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ module.exports = {
     'references-empty': [2, 'never'],
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
