cde6e8a378fa6f5ed2d6050445664858bab8c31a
dkk94 <davidkurniakristiadi@gmail.com>
2020-04-18 23:48:07 +0700

    chore(docs): improve meta description

--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Amar UI Web',
     description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+      '⚡️CSS framework of components, layouts and utilities based on Amar Design System',
     author: `David Kurnia Kristiadi @dkk94`,
   },
   plugins: [
