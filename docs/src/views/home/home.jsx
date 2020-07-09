9646f0cd215a6621d32982c320f26c43b7b0320b
dkk94 <davidkurniakristiadi@gmail.com>
2020-07-09 23:45:00 +0700

    docs(home): rename landing title

--- docs/src/views/home/home.jsx
+++ docs/src/views/home/home.jsx
@@ -19,13 +19,10 @@ const Home = () => (
               <div className="Grid Grid-6">
                 <div className="Flex-column AlignItems-center MarginRight">
                   <div className="MarginBottom-large">
-                    <h1 className="FontSize-5xlarge MarginBottom">
-                      Amar UI CSS
-                    </h1>
+                    <h1 className="FontSize-5xlarge MarginBottom">Amar UI Web</h1>
                     <p className="FontSize-2xlarge Color-neutral-20">
-                      An <strong>open-source</strong> CSS Framework for crafting
-                      beautiful user interfaces and experiences with Amar Design
-                      System as its foundation.
+                      An <strong>open-source</strong> CSS Framework for crafting beautiful user
+                      interfaces and experiences with Amar Design System as its foundation.
                     </p>
                   </div>
                   <Link
