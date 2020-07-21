e0f5298e9896e3ee2500f7d52be4a1f1abe3ce7c
dkk94 <davidkurniakristiadi@gmail.com>
2020-07-21 23:51:10 +0700

    docs(code-preview): use CodePreviewContainer as wrapper

--- docs/src/components/code-preview/code-preview.jsx
+++ docs/src/components/code-preview/code-preview.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import JsxParser from 'react-jsx-parser';
-import { CodePreviewDisplay, CodePreviewBlock } from '../code-preview';
+import { CodePreviewContainer, CodePreviewDisplay, CodePreviewBlock } from '../code-preview';
 import './code-preview.scss';
 
 const CodePreview = props => {
@@ -11,17 +11,13 @@ const CodePreview = props => {
     return <JsxParser renderInWrapper={false} jsx={string} />;
   };
 
-  const cssClasses = {
-    codePreview: classNames(`CodePreview`),
-  };
-
   return (
-    <div className={cssClasses['codePreview']}>
+    <CodePreviewContainer>
       <CodePreviewDisplay fullWidth={fullWidth} bgColor={bgColor}>
         {parseCode(codeString)}
       </CodePreviewDisplay>
       <CodePreviewBlock {...props} />
-    </div>
+    </CodePreviewContainer>
   );
 };
 
