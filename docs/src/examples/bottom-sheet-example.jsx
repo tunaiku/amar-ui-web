134ce8a66cf22fb17a51c9346aafd45f0d949e6c
dkk94 <davidkurniakristiadi@gmail.com>
2020-08-18 23:54:02 +0700

    docs(bottom-sheet): improve bottom sheet example with the latest classes

--- docs/src/examples/bottom-sheet-example.jsx
+++ docs/src/examples/bottom-sheet-example.jsx
@@ -14,22 +14,18 @@ const BottomSheetExample = () => {
         <div className="BottomSheet">
           <div className="BottomSheet-container">
             <div className="BottomSheet-dialog">
-              <div className="BottomSheet-block PaddingTop-xlarge PaddingBottom-xlarge">
-                <div className="Flex JustifyContent-center">
-                  <img
-                    alt="BottomSheetExample"
-                    src="https://assets-global.website-files.com/5bfd1275cc56e15ce750b18e/5d0521f085f49b4ae4a43c79_28.png"
-                    style={{ width: '150px' }}
-                  />
-                </div>
+              <div className="BottomSheet-media">
+                <img
+                  alt="BottomSheetExample"
+                  src="https://res.cloudinary.com/dwhvwfzcm/image/upload/v1593543146/undraw_working_from_anywhere_ub67_wg7r8a.svg"
+                  style={{ width: '150px' }}
+                />
               </div>
-              <div className="BottomSheet-block">
-                <h5 className="MarginBottom-2xsmall">
-                  Selamat, Pinjaman Anda telah lunas!
-                </h5>
+              <div className="BottomSheet-body">
+                <h5 className="BottomSheet-title">Selamat, Pinjaman Anda telah lunas!</h5>
                 <p className="Color-gray-50 MarginBottom">
-                  Terima kasih telah menggunakan pinjaman Tunaiku. Surat
-                  keterangan lunas akan dikirim ke alamat Anda.
+                  Terima kasih telah menggunakan pinjaman Tunaiku. Surat keterangan lunas akan
+                  dikirim ke alamat Anda.
                 </p>
                 <button className="Button Button--fullWidth Button--success">
                   Ajukan Pinjaman Lagi
@@ -37,10 +33,7 @@ const BottomSheetExample = () => {
               </div>
             </div>
           </div>
-          <span
-            className="BottomSheet-backdrop"
-            role="button"
-            onClick={toggleBottomSheet}></span>
+          <span className="BottomSheet-backdrop" role="button" onClick={toggleBottomSheet}></span>
         </div>
       )}
     </div>
