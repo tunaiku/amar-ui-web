7980c2fa97b8a8c53a22bda2e457c664a19e8065
dkk94 <davidkurniakristiadi@gmail.com>
2020-09-10 23:56:40 +0700

    docs(snackbar): add snackbar example to preview

---
+++ docs/src/docs/components/snackbar.example.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SnackbarExample = () => {
+  return (
+    <div class="Snackbar-dialog">
+      <div class="Snackbar-text">A single line message.</div>
+      <div class="Snackbar-actions">
+        <button class="Snackbar-action">Action</button>
+      </div>
+    </div>
+  );
+};
+
+export default SnackbarExample;
