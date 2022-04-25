7d40048449c499e7ca1d2b9bdae2c07b54957f05
dkk94 <davidkurniakristiadi@gmail.com>
2022-04-25 15:51:34 +0700

    chore: [IF-138] Import font-faces to typography variables

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ module.exports = env => {
               loader: 'css-loader',
               options: {
                 sourceMap: true,
+                url: false,
               },
             },
             {
